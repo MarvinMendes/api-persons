b775ed66462d80430b1863752fe6bd3af304a005
Marvin Mendes <marvin.m.lopes@gmail.com>
2021-06-30 08:09:51 -0300

    Redefinindo a ordem dos plugins do lombok e mapstruct para o funcionamento devido.

--- src/main/java/com/dio/api/persons/apipersons/mapper/PersonMapper.java
+++ src/main/java/com/dio/api/persons/apipersons/mapper/PersonMapper.java
@@ -8,9 +8,10 @@ import org.mapstruct.factory.Mappers;
 
 @Mapper
 public abstract class PersonMapper {
+
     public static final PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
 
-    @Mapping(target = "birthDate", source = "birthDate", dateFormat = "dd/MM/yyyy")
+    @Mapping(source = "birthDate", target = "birthDate", dateFormat = "dd/MM/yyyy")
     public abstract Person toEntity(PersonDTO dto);
 
     public abstract PersonDTO toDTO(Person person);
