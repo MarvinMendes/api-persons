f6e9eb75aeefdcb032a1081e7c53baac95ff81a9
Marvin Mendes <marvin.m.lopes@gmail.com>
2021-06-30 06:23:21 -0300

    Corrigindo a dependência do mapstruct no pom.xml e outros pequenos ajustes.

--- src/main/java/com/dio/api/persons/apipersons/mapper/PersonMapper.java
+++ src/main/java/com/dio/api/persons/apipersons/mapper/PersonMapper.java
@@ -3,14 +3,12 @@ package com.dio.api.persons.apipersons.mapper;
 import com.dio.api.persons.apipersons.domain.Person;
 import com.dio.api.persons.apipersons.dto.PersonDTO;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public abstract class PersonMapper {
     public static final PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
 
-    @Mapping(target = "birthDate", source = "birthDate", dateFormat = "dd-MM-yyyy")
     public abstract Person toEntity(PersonDTO dto);
 
     public abstract PersonDTO toDTO(Person person);
